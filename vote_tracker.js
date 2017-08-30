53d4c480da596f8606c23385375b32e9929685aa
tanyagriego <tanya.griego@gmail.com>
2017-08-29 18:53:24 -0700

    fixed images links

--- vote_tracker.js
+++ vote_tracker.js
@@ -1,25 +1,27 @@
-var imageNames = ["bad.jpg","banana.jpg",""]
+var imageNames = ["bad.jpg","banana.jpg","boots.jpg","chair.jpg",
+"cthulhu.jpg","dragon.jpg","pen.jpg","scissors.jpg","shark.jpg","sweep.jpg",
+"unicorn.jpg","usb.jpg","water_can.jpg","wine_glass.jpg"]
 
 function addImage(imageFileName) {
   var container = document.getElementById ("image-container");
   var image = document.createElement("img");
   image.src = imageFileName;
   image.addEventListener("click", recordClick);
-  container.appendChild(imgae);
+  container.appendChild(image);
 }
 
 function showImages() {
   var index = Math.floor(Math.random() * 14)
   addImage("images/" +imageNames[index]);
-  var index = Math.floor(Math.random() * 14)
+  index = Math.floor(Math.random() * 14)
   addImage("images/" +imageNames[index]);
-  var index = Math.floor(Math.random() * 14)
+  index = Math.floor(Math.random() * 14)
   addImage("images/" +imageNames[index]);
 }
 
 function recordClick(event) {
   var imageSource = event.target.src;
-  console.log("Image Clicked "+ imageSource!);
+  console.log("Image Clicked "+ imageSource);
 }
 
 window.addEventListener("load", showImages);
