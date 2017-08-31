9faa119d20765b8c84022ced3f18a4f6b0db8848
tanyagriego <tanya.griego@gmail.com>
2017-08-31 08:44:58 -0700

    added object cont., updated CSS

--- vote_tracker.js
+++ vote_tracker.js
@@ -1,38 +1,57 @@
-var imageNames = ["bag.jpg","banana.jpg","boots.jpg","chair.jpg",
-"cthulhu.jpg","dragon.jpg","pen.jpg","scissors.jpg","shark.jpg","sweep.jpg",
-"unicorn.jpg","usb.jpg","water_can.jpg","wine_glass.jpg"]
+var ImageOption = function (name, tally, fileName) {
+  this.name = name
+  this.tally = 0
+  this.fileName = name + ".jpg"
+}
 
-function addImage(imageFileName) {
+imageNames = [];
+imageNames.push(new ImageOption("bag"));
+imageNames.push(new ImageOption("banana"));
+imageNames.push(new ImageOption("boots"));
+imageNames.push(new ImageOption("chair"));
+imageNames.push(new ImageOption("cthulhu"));
+imageNames.push(new ImageOption("dragon"));
+imageNames.push(new ImageOption("pen"));
+imageNames.push(new ImageOption("scissors"));
+imageNames.push(new ImageOption("shark"));
+imageNames.push(new ImageOption("sweep"));
+imageNames.push(new ImageOption("unicorn"));
+imageNames.push(new ImageOption("usb"));
+imageNames.push(new ImageOption("water_can"));
+imageNames.push(new ImageOption("wine_glass"));
+
+
+
+function addImage(imageObject) {
   var container = document.getElementById ("image-container");
   var image = document.createElement("img");
-  image.src = imageFileName;
+  image.src = "images/" + imageObject.fileName;
   image.addEventListener("click", recordClick);
   container.appendChild(image);
 }
 
 function showImages() {
   var index = Math.floor(Math.random() * 14)
-  addImage("images/" +imageNames[index]);
+  addImage(imageNames[index]);
 
   var indexTwo = Math.floor(Math.random() * 14)
   while (index == indexTwo) {
     indexTwo = Math.floor(Math.random() * 14)
   }
-  addImage("images/" +imageNames[indexTwo]);
+  addImage(imageNames[indexTwo]);
 
   var indexThree = Math.floor(Math.random() * 14)
   while (indexTwo == indexThree || index == indexThree) {
     indexThree = Math.floor(Math.random() * 14)
   }
-  addImage("images/" +imageNames[indexThree]);
+  addImage(imageNames[indexThree]);
 
 }
 
-
 function recordClick(event) {
   var imageSource = event.target.src;
+  // imageNames this.tally +
   console.log("Image Clicked "+ imageSource);
 }
 
 window.addEventListener("load", showImages);
-// addEventListener("click")
