ea274de79482ab3f45da99ffa2d43307646ead58
tanyagriego <tanya.griego@gmail.com>
2017-08-31 20:15:43 -0700

    added tracker and modified css

--- vote_tracker.js
+++ vote_tracker.js
@@ -1,7 +1,7 @@
-var ImageOption = function (name, tally, fileName) {
-  this.name = name
-  this.tally = 0
-  this.fileName = name + ".jpg"
+var ImageOption = function (name, tally) {
+  this.name = name;
+  this.tally = 0;
+  this.fileName = name + ".jpg";
 }
 
 imageNames = [];
@@ -19,13 +19,24 @@ imageNames.push(new ImageOption("unicorn"));
 imageNames.push(new ImageOption("usb"));
 imageNames.push(new ImageOption("water_can"));
 imageNames.push(new ImageOption("wine_glass"));
+// console.log(imageNames);
+
+// var tableBody =
+// document.getElementById ("ImageOption-table");
+// for (var index = 0; index < ImageOption.length; index++) {
+//   ImageOption[index].recordClick ();
+//   tableBody.appendChild(ImageOption[index].getTableRowInfo());
+// }
 
 
 
-function addImage(imageObject) {
+function addImage(imageObject, index) {
   var container = document.getElementById ("image-container");
   var image = document.createElement("img");
-  image.src = "images/" + imageObject.fileName;
+  // console.log(image);
+  // console.dir(image);
+  image.src = imageObject;
+  image.dataset.index = index;
   image.addEventListener("click", recordClick);
   container.appendChild(image);
 }
@@ -33,34 +44,41 @@ function addImage(imageObject) {
 
 function showImages() {
   var index = Math.floor(Math.random() * 14)
-  addImage(imageNames[index]);
+  console.log(imageNames[index]);
+  addImage("images/" + imageNames[index].fileName, index);
 
   var indexTwo = Math.floor(Math.random() * 14)
   while (index == indexTwo) {
     indexTwo = Math.floor(Math.random() * 14)
   }
-  addImage(imageNames[indexTwo]);
+  addImage("images/" + imageNames[indexTwo].fileName, indexTwo);
 
   var indexThree = Math.floor(Math.random() * 14)
   while (indexTwo == indexThree || index == indexThree) {
     indexThree = Math.floor(Math.random() * 14)
   }
-  addImage(imageNames[indexThree]);
+  addImage("images/" + imageNames[indexThree].fileName, indexThree);
 
 }
 
-// function congratsAlert (event) {
-//   alert("You Voted!");
-//   var photoName = event.target.getElementById(image-container);
-//   alert("Awesome! You voted for " + photoName);
-// }
+
 
 function recordClick(event) {
+  alert("You voted! " + imageSource);
   var imageSource = event.target.src;
-  alert("You voted for "+ imageSource)
+  // console.log(event);
+  imageNames [event.target.dataset.index].tally++;
+  console.log(imageNames [event.target.dataset.index]);
+  // for (var index = 0; index < imageNames.length; index++) {
+  //   if (imageSource.indexOf(imageNames[index].imageSource) >= 0) {
+  //     imageNames[index].tally++;
+    // console.log("Image Clicked "+imageSource);
+    // }
+  // }
+  // alert("You voted for "+ imageSource)
   // imageNames this.tally +
   // console.log("Image Clicked "+ imageSource);
 }
 
-document.getElementById('image-container').addEventListener("click", recordClick);
+
 window.addEventListener("load", showImages);
