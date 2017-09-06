152b0bc8fb30135c991265253e5ef5f980b08e7d
tanyagriego <tanya.griego@gmail.com>
2017-09-05 18:43:14 -0700

    cha-cha-cha changes

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # market-analysis-app
+Project by Tanya Griego
+Purpose: Tracking votes for folks who wanna vote! 
---
+++ chart.js
@@ -0,0 +1,29 @@
+window.onload = function () {
+	var chart = new CanvasJS.Chart("chartContainer",
+	{
+		animationEnabled: true,
+		theme: "theme2",
+		//exportEnabled: true,
+		title:{
+			text: "Simple Column Chart"
+		},
+		data: [
+		{
+			type: "column", //change type to bar, line, area, pie, etc
+			dataPoints: [
+				{ x: 10, y: 71 },
+				{ x: 20, y: 55 },
+				{ x: 30, y: 50 },
+				{ x: 40, y: 65 },
+				{ x: 50, y: 95 },
+				{ x: 60, y: 68 },
+				{ x: 70, y: 28 },
+				{ x: 80, y: 34 },
+				{ x: 90, y: 14 }
+			]
+		}
+		]
+	});
+
+	chart.render();
+}
